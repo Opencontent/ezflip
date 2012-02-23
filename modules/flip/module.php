f7aeab5fd3f902b1ac876097d3882cd819d6af73
Luca Realdi <luca.realdi@opencontent.it>
2012-02-23 14:56:09 +0100

    Corretta latezza di deaftul del megazine Corretto definizione del module.php per la gestione dei permessi

--- modules/flip/module.php
+++ modules/flip/module.php
@@ -1,17 +1,20 @@
 <?php
-$Module = array( 'name' => 'eZFlip' );
+$Module = array( 'name' => 'eZ Flip' );
 
 $ViewList = array();
 $ViewList['flip'] = array('script' => 'flip.php',
+                          'functions' => array( 'flip' ),
 						  'ui_context' => 'navigation',
 						  'default_navigation_part' => 'ezflippart',
 						  'params' => array( 'NodeID' )
 						 );
 $ViewList['enqueue'] = array('script' => 'enqueue.php',
+                             'functions' => array( 'enqueue' ),
 						  'ui_context' => 'navigation',
 						  'params' => array( 'AttributeID', 'ContentobjectVersion', 'ObjectID', 'NodeID', 'Reflip' )
 						 );
 $ViewList['upload'] = array( 'script' => 'upload.php',
+                             'functions' => array( 'upload' ),
                              'single_post_actions' => array( 'UploadButton' => 'Upload' ),
                              'params' => array( 'ParentNodeID' ) );
 						 
